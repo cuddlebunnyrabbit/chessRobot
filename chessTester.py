e8946ded3952cce39667499862bc58d3be7151f1
Kerry Huang <kerryhuang122@gmail.com>
2022-10-02 17:53:01 -0400

    test python chess package preliminary function

---
+++ chessTester.py
@@ -0,0 +1,18 @@
+import chess
+
+board = chess.Board()
+
+board.legal_moves
+chess.Move.from_uci("a8a1") in board.legal_moves
+
+board.push_san("e4")
+board.push_san("e5")
+board.push_san("Qh5")
+board.push_san("Nc6")
+board.push_san("Bc4")
+board.push_san("Nf6")
+board.push_san("Qxf7")
+board.is_checkmate()
+board = chess.Board("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
+
+print(board)
