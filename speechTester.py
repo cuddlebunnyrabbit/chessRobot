96c43b38c7bc3a82b237dffabedbd0b75a1f2cb5
Kerry Huang <kerryhuang122@gmail.com>
2022-10-16 01:34:05 -0400

    add future tasks and speech to text tester

--- speechTester.py
+++ speechTester.py
@@ -1,5 +1,19 @@
-import speech_recognition as sr
-from pocketsphinx import LiveSpeech
+import speech_recognition as sr #pip3 install SpeechRecognition
 
+#from pocketsphinx import LiveSpeech 
+'''
 for phrase in LiveSpeech():
     print(phrase)
+'''
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print('Speak Anything:')
+    audio = r.listen(source)
+    
+    try:
+        text = r.recognize_google(audio)#convert audio to text
+        print('You said: {}'.format(text))
+
+    except:
+        print('Sorry could not recognize your voice')
