e2bf389c10192338377d3552167f9f69733b2f56
Kerry Huang <kerryhuang122@gmail.com>
2022-10-16 12:27:35 -0400

    create speechTester folder

---
+++ speechTester/commandInterpreter.py
@@ -0,0 +1,20 @@
+def interpret(voice_command):
+    pass
+
+
+def parse(voice_command):
+    listThing = [None] * 5
+    command = voice_command.lower()
+    print(len(listThing))
+    print(command)
+# parse the things into 3 different parts
+#piece, square, number 
+
+#possible combinations 
+'''
+Queen or Q 
+ex, x, X 
+
+'''
+
+parse("Bishop B4")
--- speechTester/speechTester.py
+++ speechTester/speechTester.py
@@ -8,7 +8,7 @@ for phrase in LiveSpeech():
     print(phrase)
 '''
 
-'''
+
 import speech_recognition as sr
 
 r = sr.Recognizer()
@@ -22,16 +22,24 @@ with sr.Microphone() as source:
 
     except:
         print('Sorry could not recognize your voice')
-'''
 
+
+'''
 import os
 from pocketsphinx import LiveSpeech
 
+speech = LiveSpeech()
+
+for phrase in speech:
+    print(phrase)
+
+'''
+
+'''
 speech = LiveSpeech(
     sampling_rate=16000,  # optional
     lm=get_model_path('en-us.lm.bin'), #what is this thing again? what does lm stand for again?
     dic=get_model_path('cmudict-en-us.dict')
 )
+'''
 
-for phrase in speech:
-    print(phrase)
