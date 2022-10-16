aae18c0f25c67d56227d518005e26041aef63bc2
Kerry Huang <kerryhuang122@gmail.com>
2022-10-16 02:11:40 -0400

    delete tests + add ideas

--- speechTester.py
+++ speechTester.py
@@ -1,16 +1,18 @@
 import speech_recognition as sr #pip3 install SpeechRecognition
+#speech_recognition has dependency on pyaudio! + pyaudio has dependency on portaudio
+# thus:  brew install portaudio -> pip install pyaudio
 
 #from pocketsphinx import LiveSpeech 
 '''
 for phrase in LiveSpeech():
-    print(phrase)
+    print(phrase)f
 '''
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print('Speak Anything:')
     audio = r.listen(source)
-    
+
     try:
         text = r.recognize_google(audio)#convert audio to text
         print('You said: {}'.format(text))
