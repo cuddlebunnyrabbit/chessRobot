09bbd9cb0c389612a95c0eda4e71d013d8c5c7ae
Kerry Huang <kerryhuang122@gmail.com>
2022-10-16 02:38:03 -0400

    revise training model ideas from limiting dictinary to training model

--- speechTester.py
+++ speechTester.py
@@ -1,12 +1,15 @@
-import speech_recognition as sr #pip3 install SpeechRecognition
+ #pip3 install SpeechRecognition
 #speech_recognition has dependency on pyaudio! + pyaudio has dependency on portaudio
 # thus:  brew install portaudio -> pip install pyaudio
 
 #from pocketsphinx import LiveSpeech 
 '''
 for phrase in LiveSpeech():
-    print(phrase)f
+    print(phrase)
 '''
+
+'''
+import speech_recognition as sr
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
@@ -19,3 +22,16 @@ with sr.Microphone() as source:
 
     except:
         print('Sorry could not recognize your voice')
+'''
+
+import os
+from pocketsphinx import LiveSpeech
+
+speech = LiveSpeech(
+    sampling_rate=16000,  # optional
+    lm=get_model_path('en-us.lm.bin'), #what is this thing again? what does lm stand for again?
+    dic=get_model_path('cmudict-en-us.dict')
+)
+
+for phrase in speech:
+    print(phrase)
