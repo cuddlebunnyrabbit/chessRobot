1fa468c7a430d0bca3ae523b629aa4aa09518f06
Kerry Huang <kerryhuang122@gmail.com>
2022-10-19 00:46:35 -0400

    clean up user command

---
+++ speechTester/commandGenerator.py
@@ -0,0 +1,8 @@
+pieces = ['K', 'Q', 'R', 'B', 'K', 'P']
+specialMoves =['0-0', '0-0-0', 'Promote']
+
+def generate():
+    pass 
+
+
+
--- speechTester/commandInterpreter.py
+++ speechTester/commandInterpreter.py
@@ -1,20 +1,59 @@
-def interpret(voice_command):
-    pass
+#from distutils.log import error
+
+replacement_dictionary = { #contain numbers + pieces to be replaced for notation
+    "queen": "Q",
+    "king": "K",
+    "pawn": "", #removes pawn 
+    "knight": "N",
+    "rook": "R",
+    "bishop": "B"
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "promote": "=" #this is the correct way to promote 
+}
 
 
-def parse(voice_command):
-    listThing = [None] * 5
-    command = voice_command.lower()
-    print(len(listThing))
-    print(command)
-# parse the things into 3 different parts
-#piece, square, number 
+special_dictionary = { #contain numbers + pieces to be returned immediately
+    "longcastle": "0-0-0",
+    "shortcastle": "0-0",
+    "kingsidecastle": "0-0",
+    "queensidecastle": "0-0-0",
+    
 
-#possible combinations 
-'''
-Queen or Q 
-ex, x, X 
+}
 
-'''
+def parse(command):
 
-parse("Bishop B4")
+
+    if len(command) == 0:
+        pass 
+        #throw ZeroDivisionError
+
+    # clean the voice_commands
+    while " " in command: #remove space
+        command = command.replace(' ', '')
+    command = command.lower() #lowercase
+    command = command.replace('x', '') #remove capture
+
+    for key in 
+
+    for key in pieces_dictionary:
+        if key in command:
+            command = command.replace(key, pieces_dictionary[key])
+
+    for key in numbers_dictionary:
+        if key in command:
+            command = command.replace(key, numbers_dictionary[key])
+
+    return command
+
+
+print("hello")
+stuff = parse("Bishop   x   Bfour")
+print(stuff)
