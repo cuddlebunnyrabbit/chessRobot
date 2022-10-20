be5d79184e10bbc21b1a27fe68592c193a539674
ChrisBrasil10 <115044920+ChrisBrasil10@users.noreply.github.com>
2022-10-20 10:38:34 -0400

    Update lcd_main.py

--- lcd_main.py
+++ lcd_main.py
@@ -1,4 +1,4 @@
-"""Implements a HD44780 character LCD connected via PCF8574 on I2C."""
+"""Implements user input to display"""
 
 from lcd_api import LcdApi
 from i2c_lcd import I2cLcd
@@ -9,6 +9,5 @@ I2C_NUM_COLS = 16
  
 lcd = I2cLcd(1, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
  
-lcd.putstr("Great! It Works!")
-lcd.move_to(3,1)
-lcd.putstr("freva.com")
+lcd.move_to(2,0)
+lcd.putstr("ChessBot")
