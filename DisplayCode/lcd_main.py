fb10b1001bc95fb325a0d78bcb08ee44ed7c0db6
Kerry Huang <kerryhuang122@gmail.com>
2022-10-31 00:09:18 -0400

    add lcd progress

--- DisplayCode/lcd_main.py
+++ DisplayCode/lcd_main.py
@@ -9,6 +9,7 @@ I2C_NUM_COLS = 16
 
 lcd = I2cLcd(1, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
-lcd.putstr("Welcome to")
-lcd.move_to(0, 1)
-lcd.putstr("ChessBot")
+def printMessage(messageList):
+    lcd.putstr(messageList[0])
+    lcd.move_to(0, 1)
+    lcd.putstr(messageList[1])
