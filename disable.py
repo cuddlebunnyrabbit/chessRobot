23e027db1490d458da8b3ddd66e58e1e31766ab8
RAD Group 47 <ihatetochoosemyusername@gmail.com>
2022-12-06 12:52:30 -0500

    transfer files

--- disable.py
+++ disable.py
@@ -1,4 +1,14 @@
 import motorlib
+import time
 
 motorlib.MotorSys.disable()
+motorlib.MotorSys.EMSet(False)
+'''
+motorlib.MotorSys.move_to_square("d2")
+motorlib.MotorSys.EMSet(True)
+time.sleep(5)
+motorlib.MotorSys.EMSet(False)
+motorlib.MotorSys.move_to_square("h1")
+'''
+#motorlib.MotorSys.just_move(-200, -200)
 #motorlib.MotorSys.push_move("a2", "a4", False)
